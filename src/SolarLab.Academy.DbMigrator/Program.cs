1e30cb80655b7b2beab6e39e326de801fda73352
Aleksei Tokarev <a.tokarev@solarl.ru>
2024-09-05 18:30:30 +0300

    Удалил лишние файлы

---
+++ src/SolarLab.Academy.DbMigrator/Program.cs
@@ -0,0 +1,14 @@
+namespace SolarLab.Academy.DbMigrator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = Host.CreateApplicationBuilder(args);
+            builder.Services.AddHostedService<Worker>();
+
+            var host = builder.Build();
+            host.Run();
+        }
+    }
+}
