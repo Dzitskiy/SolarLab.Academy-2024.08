d9860b0c9dfbd2f5afeb3381436bc3df120be301
Aleksei Tokarev <a.tokarev@solarl.ru>
2024-09-05 18:45:20 +0300

    Модели и конфигурации

---
+++ src/SolarLab.Academy.DataAccess/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SolarLab.Academy.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarLab.Academy.DataAccess.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).HasMaxLength(1000).IsRequired();
+
+            builder.HasMany(x => x.Adverts).WithOne(x => x.Category).HasForeignKey(x => x.CategoryId).IsRequired().OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ src/SolarLab.Academy.Domain/Advert.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarLab.Academy.Domain
+{
+    /// <summary>
+    /// Объявление.
+    /// </summary>
+    public class Advert
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public Guid CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
