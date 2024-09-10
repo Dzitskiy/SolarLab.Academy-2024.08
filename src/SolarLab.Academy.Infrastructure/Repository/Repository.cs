c414071fe21f7d65d739d38f9e93d03baeef0517
Aleksei Tokarev <a.tokarev@solarl.ru>
2024-09-10 18:32:45 +0300

    Доработка репозитория

--- src/SolarLab.Academy.Infrastructure/Repository/Repository.cs
+++ src/SolarLab.Academy.Infrastructure/Repository/Repository.cs
@@ -1,37 +1,60 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SolarLab.Academy.Infrastructure.Repository
 {
     /// <inheritdoc />
-    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    public class Repository<TEntity, TContext> : IRepository<TEntity, TContext> where TEntity : class where TContext : DbContext
     {
-        public IEnumerable<TEntity> GetAll()
+        protected TContext DbContext;
+        protected DbSet<TEntity> DbSet;
+
+        public Repository(TContext dbContext)
         {
-            throw new NotImplementedException();
+            DbContext = dbContext;
+            DbSet = DbContext.Set<TEntity>();
         }
 
-        public IEnumerable<TEntity> GetByPredicate(Func<TEntity, bool> predicate)
+        public async Task<TEntity> GetByIdAsync(Guid id, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return await DbSet.FindAsync(id, cancellationToken);
         }
 
-        public TEntity Add(TEntity model)
+        public async Task AddAsync(TEntity model, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            await DbSet.AddAsync(model, cancellationToken);
+            await DbContext.SaveChangesAsync(cancellationToken);
         }
 
-        public TEntity Update(TEntity model)
+        public async Task UpdateAsync(TEntity model, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            DbSet.Update(model);
+            await DbContext.SaveChangesAsync(cancellationToken);
         }
 
-        public bool Delete(Guid id)
+        public async Task DeleteAsync(Guid id, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var entity = await GetByIdAsync(id, cancellationToken);
+            if (entity != null)
+            {
+                DbSet.Remove(entity);
+                await DbContext.SaveChangesAsync(cancellationToken);
+            }
+        }
+
+        IQueryable<TEntity> IRepository<TEntity, TContext>.GetAll()
+        {
+            return DbSet;
+        }
+
+        IQueryable<TEntity> IRepository<TEntity, TContext>.GetByPredicate(Expression<Func<TEntity, bool>> predicate)
+        {
+            return DbSet.Where(predicate);
         }
     }
 }
