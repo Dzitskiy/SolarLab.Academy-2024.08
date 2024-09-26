f7ec64d5a5381bb86bcabaa75d4030757c72301d
Andreev Aleksey <AAndreevEx@alfabank.ru>
2024-09-26 09:21:45 +0300

    Tests for service

--- tests/SolarLab.Academy.Tests/AppServicesTests/Contexts/Adverts/Services/AdvertServiceTests.cs
+++ tests/SolarLab.Academy.Tests/AppServicesTests/Contexts/Adverts/Services/AdvertServiceTests.cs
@@ -1,11 +1,12 @@
 ﻿using AutoFixture;
 using AutoMapper;
-using Microsoft.Extensions.Configuration;
 using Moq;
 using SolarLab.Academy.AppServices.Contexts.Adverts.Builders;
 using SolarLab.Academy.AppServices.Contexts.Adverts.Repositories;
 using SolarLab.Academy.AppServices.Contexts.Adverts.Services;
 using SolarLab.Academy.ComponentRegistrar.MapProfiles;
+using SolarLab.Academy.Contracts.Contexts.Adverts.Requests;
+using SolarLab.Academy.Domain;
 using Xunit;
 
 namespace SolarLab.Academy.Tests.AppServicesTests.Contexts.Adverts.Services;
@@ -19,6 +20,8 @@ public class AdvertServiceTests
     private readonly Mock<IAdvertRepository> _advertRepositoryMock;
     private readonly Mock<IAdvertSpecificationBuilder> _advertSpecificationBuilderMock;
     private readonly IMapper _mapper;
+    private readonly Fixture _fixture;
+    private readonly CancellationToken _token;
 
     public AdvertServiceTests()
     {
@@ -35,15 +38,24 @@ public class AdvertServiceTests
         configurationProvider.AssertConfigurationIsValid();
         _mapper = configurationProvider.CreateMapper();
         _service = new AdvertService(_advertRepositoryMock.Object, _advertSpecificationBuilderMock.Object, _mapper);
+        _fixture = new Fixture();
+
+        // TODO: рассказать про отличая new CancellationTokenSource().Token и CancellationToken.None
+        _token = new CancellationTokenSource().Token;
     }
 
     [Fact]
     public async Task CreateAsync_Should_Call_RepositoryMethod_Async()
     {
         // Arrange
+        var request = _fixture.Create<CreateAdvertRequest>();
+        var advertId = _fixture.Create<Guid>();
 
         // Act
-
+        var result = await _service.CreateAsync(request, _token);
+        
         // Assert
+        //TODO: как это исправить и что мы ожидаем как результат работы теста?
+        Assert.NotEqual(Guid.Empty, result);
     }
 }
